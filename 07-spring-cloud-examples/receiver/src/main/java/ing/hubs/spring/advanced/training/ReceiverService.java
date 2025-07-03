d626a1bfc0f1df1620e7d2215dddaffb52849f60
Bogdan Solga <bogdan.solga@gmail.com>
2025-07-03 15:15:18 +0300

    [dev] Added the hands-on work modules

---
+++ 07-spring-cloud-examples/receiver/src/main/java/ing/hubs/spring/advanced/training/ReceiverService.java
@@ -0,0 +1,12 @@
+package ing.hubs.spring.advanced.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiverService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiverService.class, args);
+    }
+}
---
+++ 07-spring-cloud-examples/requester/src/main/java/ing/hubs/spring/advanced/training/RequesterService.java
@@ -0,0 +1,12 @@
+package ing.hubs.spring.advanced.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequesterService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequesterService.class, args);
+    }
+}
